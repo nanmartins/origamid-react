ba600f50b6c56ab1192bd59a947e7b1cee0773e9
Renan Martins <renanmartins989@gmail.com>
2022-11-22 12:23:25 +0000

    and add modal component to home

--- src/Ex components/Home.js
+++ src/Ex components/Home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Titulo from './Titulo'
+import App from '../Ex hooks & props/App'
 
 const Home = () => {
   return (
     <React.Fragment>
       <Titulo texto="Home" />
       <p>Essa e a home do site</p>
+      <App />
     </React.Fragment>
   )
 }
