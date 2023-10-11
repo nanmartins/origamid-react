ef008c5606d750ad2a9245f3dc96526690e73897
nanmartins <renanmartins989@gmail.com>
2023-10-11 03:58:47 +0100

    ....

---
+++ 02/0204/src/App.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const App = () => {
+  return <a href="#">Link</a>
+}
+
+export default App
---
+++ 02/0204/src/main.jsx
@@ -0,0 +1,13 @@
+import ReactDOM from 'react-dom/client'
+import React from 'react'
+import App from './App'
+
+// function App() {
+//   return <h1>Hello!!!</h1>
+// }
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
