f68a0e89c7401f4113b35a557831b4be3f8c1dd8
nanmartins <renanmartins989@gmail.com>
2023-10-11 03:57:47 +0100

    ...

---
+++ 02/0205/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+const App = () => {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      App React
+    </div>
+  )
+}
+
+export default App
