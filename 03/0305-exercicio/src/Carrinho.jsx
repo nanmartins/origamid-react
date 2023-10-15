666ddabbcfc46bafbfcb7fb10703d8f9f0044787
nanmartins <renanmartins989@gmail.com>
2023-10-15 04:54:35 +0100

    ...

--- 03/0305-exercicio/src/Carrinho.jsx
+++ 03/0305-exercicio/src/Carrinho.jsx
@@ -3,15 +3,29 @@ import { GlobalContext } from './GlobalContext'
 
 const Carrinho = () => {
 
+  const liStyle = {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    border: '1px solid',
+    margin: '10px',
+    padding: '0 10px'
+  }
+
   // global context desestruturado
-  const {cart, limparCart} = React.useContext(GlobalContext)
+  const {cart, setCart, limparCart} = React.useContext(GlobalContext)
 
   return (
     <div>
       <h1>Carrinho: {cart.length}</h1>
       {cart.map((produto, index) => (
-        <li key={index}>{produto.nome}</li>
+        <li key={index} style={liStyle}>
+          <h4>{`${index + 1}  ${produto.nome}`}</h4>
+          <h5>R${produto.preco}</h5>
+          {/* <button >Remover</button> */}
+        </li>
       ))}
+      {/* {console.log(cart)} */}
 
       <button onClick={() => limparCart()}>Limpar Cart</button>
     </div>
--- 03/0305-exercicio/src/Produtos.jsx
+++ 03/0305-exercicio/src/Produtos.jsx
@@ -3,19 +3,43 @@ import { GlobalContext } from './GlobalContext'
 
 const Produtos = () => {
 
+  const liStyle = {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    border: '1px solid',
+    margin: '10px',
+    padding: '0 10px'
+  }
+
+  const btnStyle = {
+    padding: '10px 20px',
+    background: 'white',
+    margin: '10px'
+  }
+
   const global = React.useContext(GlobalContext)
 
   if(global.dados.length === 0) {
     return <h1>Loading...</h1>
   }
   return (
-    <div>
-      {global.dados.map((produto) => (
-        <li key={produto.id}>{produto.nome} <button onClick={() => global.setCart([...global.cart, produto])}>Add Cart</button></li>
-      ))}
+    <div style={{ display: 'flex', flexDirection: 'column' }}>
+      <ul style={{ padding: '0'}}>
+        {global.dados.map((produto) => (
+          <li style={liStyle} key={produto.id}>
+            <h2>{produto.nome}</h2>
+            <div style={{ display: 'flex', gap: '10px', alignItems: 'center'}}>
+              <h5 style={{ fontSize: '14px'}}>R${produto.preco}</h5>
+              <button style={btnStyle} onClick={() => global.setCart([...global.cart, produto])}>Add Cart</button>
+            </div>
+          </li>
+        ))}
+      </ul>
+      <hr style={{ margin: '0 10px'}}/>
 
       <br />
-      <button onClick={() => global.limparDados()}>Limpar Dados</button>
+      <button style={btnStyle} onClick={() => global.limparDados()}>Limpar Dados</button>
     </div>
   )
 }
