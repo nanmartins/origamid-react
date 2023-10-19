36995647c05a1fe0ff133f5215ceb33ae6597cef
nanmartins <renanmartins989@gmail.com>
2023-10-19 15:14:28 +0100

    ...

--- 04/0406/src/App.jsx
+++ 04/0406/src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Input from './Form/Input'
 import Select from './Form/Select'
+import Radio from './Form/Radio'
 
 function App() {
 
   const [nome, setNome] = React.useState('')
   const [email, setEmail] = React.useState('')
   const [produto, setProduto] = React.useState('')
+  const [cor, setCor] = React.useState('')
+  const [frutas, setFrutas] = React.useState([])
 
   return (
     <>
@@ -16,6 +19,24 @@ function App() {
 
         <div>
           <Select options={['Notebook', 'Smartphone', 'Tablet']} value={produto} setValue={setProduto}/>
+        </div>
+
+        <div>
+          <h3>Cores:</h3>
+          <Radio
+            options={['Azul', 'Vermelho']}
+            value={cor}
+            setValue={setCor}
+          />
+
+          <br />
+
+          <h3>Frutas:</h3>
+          <Radio
+            options={['Maçã', 'Uva', 'Limão']}
+            value={frutas}
+            setValue={setFrutas}
+          />
         </div>
 
         <button type="submit" style={{margin: '15px auto 0 auto'}}>Enviar</button>
