9c5f62ea389ff931cddedffbcc0fb391c4d83aa3
nanmartins <renanmartins989@gmail.com>
2023-10-20 02:22:40 +0100

    ...

--- 04/0406/src/App.jsx
+++ 04/0406/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Input from './Form/Input'
 import Select from './Form/Select'
 import Radio from './Form/Radio'
+import Checkbox from './Form/Checkbox'
 
 function App() {
 
@@ -9,13 +10,85 @@ function App() {
   const [email, setEmail] = React.useState('')
   const [produto, setProduto] = React.useState('')
   const [cor, setCor] = React.useState('')
-  const [frutas, setFrutas] = React.useState([])
+  const [frutas, setFrutas] = React.useState('')
+  const [linguagens, setLinguagens] = React.useState([])
+  const [termos, setTermos] = React.useState([])
+  const [cep, setCep] = React.useState('')
+  const [error, setError] = React.useState(null)
+
+
+  const validateCep = ((value) => {
+    if(value.length === 0) {
+      setError('Preencha um valor.')
+      return false
+    } else if (!/^\d{5}-?\d{3}$/.test(value)) {
+      setError('CEP inválido.')
+      return false
+    } else {
+      setError(null)
+      return true
+    }
+  })
+
+  const handleBlur = (({target}) => {
+    validateCep(target.value)
+  })
+
+  const handleChange = (({target}) => {
+    if(error) validateCep(target.value)
+    setCep(target.value)
+  })
+
+  const handleSubmit = ((e) => {
+    e.preventDefault()
+    if(validateCep(cep)) {
+      console.log('enviou')
+    } else {
+      console.log('erro')
+    }
+  })
+
+
+  if(termos.length > 0) {
+    console.log('Enviar')
+  }
 
   return (
     <>
+    <div>
+    <form onSubmit={handleSubmit}>
+      <Input
+        type="text"
+        label="CEP"
+        id="cep"
+        value={cep}
+        setValue={setCep}
+        onBlur={handleBlur}
+        onChange={handleChange}
+        placeholder="00000-000"
+      />
+      {error && <p>{error}</p>}
+      <br />
+      <button onClick={handleSubmit}>Enviar</button>
+      </form>
+    </div>
+
+
       <form style={{width: '175px'}}>
-        <Input id="nome" label="Nome" value={nome} setValue={setNome} required />
-        <Input id="email" label="Email" value={email} setValue={setEmail} required />
+        <Input
+          id="nome"
+          label="Nome"
+          value={nome}
+          setValue={setNome}
+          required
+        />
+        <Input
+          id="email"
+          label="Email"
+          value={email}
+          setValue={setEmail}
+          required
+        />
 
         <div>
           <Select options={['Notebook', 'Smartphone', 'Tablet']} value={produto} setValue={setProduto}/>
@@ -36,6 +109,21 @@ function App() {
             options={['Maçã', 'Uva', 'Limão']}
             value={frutas}
             setValue={setFrutas}
+          />
+        </div>
+
+        <div>
+          <h3>Checkbox</h3>
+          <Checkbox
+            options={['JavaScript', 'React', 'VueJS']}
+            value={linguagens}
+            setValue={setLinguagens}
+          />
+          <br />
+          <Checkbox
+            options={['Li e aceito os termos.']}
+            value={termos}
+            setValue={setTermos}
           />
         </div>
 
--- 04/0406/src/Form/Radio.jsx
+++ 04/0406/src/Form/Radio.jsx
@@ -4,16 +4,18 @@ const Radio = ({options, value, setValue, ...props }) => {
   return (
     <>
       {options.map(option => (
-        <label key={option}>
-          <input
-            type="radio"
-            value={option}
-            checked={value === option}
-            onChange={({target}) => setValue(target.value)}
-            {...props}
-          />
-          {option}
-        </label>
+        <div key={option}>
+          <label>
+            <input
+              type="radio"
+              value={option}
+              checked={value === option}
+              onChange={({target}) => setValue(target.value)}
+              {...props}
+            />
+            {option}
+          </label>
+        </div>
       ))}
     </>
   )
