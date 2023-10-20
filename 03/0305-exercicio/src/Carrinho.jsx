5dfe5efe74495c73a0e3270e41f1e1fbaaad8e4e
Renan Martins <91997504+nanmartins@users.noreply.github.com>
2023-10-20 13:25:18 +0100

    ...

--- 03/0305-exercicio/src/Carrinho.jsx
+++ 03/0305-exercicio/src/Carrinho.jsx
@@ -12,22 +12,38 @@ const Carrinho = () => {
     padding: '0 10px'
   }
 
+  const btnStyle = {
+    padding: '10px 20px',
+    background: 'white',
+    margin: '10px'
+  }
+
+  const divStyle = {
+    display: 'flex',
+    alignItems: 'center',
+    gap: '10px'
+  }
   // global context desestruturado
   const {cart, setCart, limparCart} = React.useContext(GlobalContext)
+  const produtoId = ''
 
   return (
     <div>
       <h1>Carrinho: {cart.length}</h1>
       {cart.map((produto, index) => (
         <li key={index} style={liStyle}>
-          <h4>{`${index + 1}  ${produto.nome}`}</h4>
-          <h5>R${produto.preco}</h5>
-          {/* <button >Remover</button> */}
+          <div style={divStyle}>
+            <img src={produto.fotos[0].src} height="50px" width="50px" alt="" />
+            <h4>{`${index + 1}  ${produto.nome}`}</h4>
+          </div>
+
+          <div style={divStyle}>
+            <h5>R${produto.preco}</h5>
+          </div>
         </li>
       ))}
-      {/* {console.log(cart)} */}
 
-      <button onClick={() => limparCart()}>Limpar Cart</button>
+      <button style={btnStyle} onClick={() => limparCart()}>Limpar Cart</button>
     </div>
   )
 }
