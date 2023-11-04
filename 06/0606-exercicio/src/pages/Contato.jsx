1d2e1852049fa8b3eabbeb48bcb5576375748829
Renan Martins <91997504+nanmartins@users.noreply.github.com>
2023-11-04 23:59:10 +0000

    ...

--- 06/0606-exercicio/src/pages/Contato.jsx
+++ 06/0606-exercicio/src/pages/Contato.jsx
@@ -4,11 +4,11 @@ import Img from '../img/contato.jpg'
 const Contato = () => {
   return (
     <section style={{ display: 'flex'}}>
-      <img src={Img} width="350px" />
+      <img src={Img} width="350px" style={{borderRadius: '8px'}} />
 
-      <div>
-        <h3>Entre em contato.</h3>
-        <ul>
+      <div style={{padding: '10px 30px'}}>
+        <h2>Entre em contato.</h2>
+        <ul style={{listStyle: 'none'}}>
           <li>contato@email.com</li>
           <li>12345-678</li>
           <li>Rua tal, 999</li>
