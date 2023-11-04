0e46daf8e9a6e5100225a2f5a1f79a3db8a11b45
nanmartins <renanmartins989@gmail.com>
2023-11-04 05:34:57 +0000

    setup para iniciar o exercicio

---
+++ 06/0606-exercicio/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+// Utilize a API abaixo para puxar a lista de produto
+// https://ranekapi.origamid.dev/json/api/produto
+// Cada produto possui o id, o mesmo pode ser passado na api para retornar os dados desse produto específico
+// https://ranekapi.origamid.dev/json/api/produto/notebook
+
+
+function App() {
+
+  return (
+    <>
+      <h1>App</h1>
+    </>
+  )
+}
+
+export default App
