1dc5e3ebbb1291f909501e0ce745d8a4ba7ba7c8
nanmartins <renanmartins989@gmail.com>
2023-11-29 10:08:54 +0000

    UserStorage and UserContext done

--- dogs/src/App.jsx
+++ dogs/src/App.jsx
@@ -4,21 +4,24 @@ import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
 import Home from './views/Home/Home'
 import Login from './views/Login/Login'
+import { UserStorage } from './contexts/UserContext'
 
 function App() {
 
   return (
     <>
       <BrowserRouter>
-        <Header />
-        <Routes>
+        <UserStorage>
+          <Header />
+          <Routes>
 
-          <Route path="/" element={ <Home /> } />
-          <Route path="/login/*" element={ <Login /> } />
+            <Route path="/" element={ <Home /> } />
+            <Route path="/login/*" element={ <Login /> } />
 
-        </Routes>
+          </Routes>
 
-        <Footer />
+          <Footer />
+        </UserStorage>
       </BrowserRouter>
 
     </>
--- dogs/src/components/Login/LoginForm.jsx
+++ dogs/src/components/Login/LoginForm.jsx
@@ -2,41 +2,19 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import Input from '../Forms/Input'
 import Button from '../Forms/Button'
-import useForm from '../../hooks/useForm'
-import { TOKEN_POST, USER_GET } from '../../data/data'
+import useForm from '../../hooks/UseForm'
+import { UserContext } from '../../contexts/UserContext'
 
 const LoginForm = () => {
 
   const username = useForm()
   const password = useForm()
-
-  React.useEffect(() => {
-    const token = window.localStorage.getItem('token')
-    if(token) {
-      getUser(token)
-    }
-  }, [])
-
-  async function getUser(token) {
-    const {url, options} = USER_GET(token)
-    const response = await fetch(url, options)
-    const json = await response.json()
-    console.log(json)
-  }
+  const { userLogin } = React.useContext(UserContext)
 
   async function handleSubmit(event) {
     event.preventDefault()
-
     if(username.validate() && password.validate()) {
-      const {url, options} = TOKEN_POST({
-        username: username.value,
-        password: password.value
-      })
-
-      const response = await fetch(url, options)
-      const json = await response.json()
-      window.localStorage.setItem('token', json.token)
-      getUser(json.token)
+      userLogin(username.value, password.value)
     }
   }
 
