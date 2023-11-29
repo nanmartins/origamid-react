d01e9b30d9eecc3e5b5bba8557b7a39719186795
nanmartins <renanmartins989@gmail.com>
2023-11-29 08:15:13 +0000

    api fetch saving token at localstorage

--- dogs/src/components/Login/LoginForm.jsx
+++ dogs/src/components/Login/LoginForm.jsx
@@ -2,35 +2,43 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import Input from '../Forms/Input'
 import Button from '../Forms/Button'
-import useForm from '../../Hooks/useForm'
+import useForm from '../../hooks/useForm'
+import { TOKEN_POST, USER_GET } from '../../data/data'
 
 const LoginForm = () => {
 
   const username = useForm()
   const password = useForm()
 
-  const handleSubmit = (event) => {
+  React.useEffect(() => {
+    const token = window.localStorage.getItem('token')
+    if(token) {
+      getUser(token)
+    }
+  }, [])
+
+  async function getUser(token) {
+    const {url, options} = USER_GET(token)
+    const response = await fetch(url, options)
+    const json = await response.json()
+    console.log(json)
+  }
+
+  async function handleSubmit(event) {
     event.preventDefault()
 
     if(username.validate() && password.validate()) {
-      console.log('Enviado')
-
-      fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify()
-      }).then(response => {
-        console.log(response)
-        return response.json()
-      }).then(json => {
-        console.log(json)
-        return json
+      const {url, options} = TOKEN_POST({
+        username: username.value,
+        password: password.value
       })
+
+      const response = await fetch(url, options)
+      const json = await response.json()
+      window.localStorage.setItem('token', json.token)
+      getUser(json.token)
     }
   }
-
 
   return (
     <section>
@@ -41,7 +49,7 @@ const LoginForm = () => {
         <Input label="Senha" type="password" name="password" {...password} />
 
         <Button>Entrar</Button>
-        {/* <button>Entrar</button> */}
+
       </form>
 
       <Link to="/login/criar">Criar conta</Link>
