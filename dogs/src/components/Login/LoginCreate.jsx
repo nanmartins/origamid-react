b424d1b676ddc289666cf5ea61b3b73e05e0d2f7
nanmartins <renanmartins989@gmail.com>
2023-12-02 09:50:37 +0000

    login create done

--- dogs/src/components/Login/LoginCreate.jsx
+++ dogs/src/components/Login/LoginCreate.jsx
@@ -1,12 +1,41 @@
 import React from 'react'
 import Input from '../Forms/Input'
+import Button from '../Forms/Button'
+import UseForm from '../../hooks/UseForm'
+import { USER_POST } from '../../data/Data'
+import { UserContext } from '../../contexts/UserContext'
 
 const LoginCreate = () => {
-  return (
-    <section>
-      <div>LoginCreate</div>
 
-      <Input />
+  const username = UseForm()
+  const email = UseForm('email')
+  const password = UseForm()
+
+  const {userLogin} = React.useContext(UserContext)
+
+  async function handleSubmit(event) {
+    event.preventDefault()
+    const {url, options} = USER_POST({
+      username: username.value,
+      email: email.value,
+      password: password.value
+    })
+    const response = await fetch(url, options)
+    if(response.ok) userLogin(username.value, password.value)
+    // const json = await response.json()
+  }
+
+  return (
+    <section className='animeLeft'>
+      <h1 className='title'>Cadastre-se</h1>
+
+      <form onSubmit={handleSubmit}>
+        <Input label="Usuário" type="text" name="username" {...username} />
+        <Input label="Email" type="email" name="email" {...email} />
+        <Input label="Senha" type="password" name="password" {...password} />
+        <Button>Cadastrar</Button>
+      </form>
+
     </section>
   )
 }
--- dogs/src/hooks/UseForm.jsx
+++ dogs/src/hooks/UseForm.jsx
@@ -4,6 +4,10 @@ const types = {
   email: {
     regex: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
     message: 'Preencha um email válido'
+  },
+  password: {
+    regex: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/,
+    message: 'A senha precisa ter 1 caracter maiúsculo, 1 minúsculo e 1 digito. Com no mínimo 8 caracteres.'
   }
 }
 
