709e5e6b06a0e04df1248851918a2fa2726fd86b
Renan Martins <renanmartins989@gmail.com>
2023-12-09 15:12:28 +0000

    footer done

--- dogs/src/App.jsx
+++ dogs/src/App.jsx
@@ -14,33 +14,34 @@ import NotFound from './components/Error404/NotFound'
 function App() {
 
   return (
-    <>
+    <div className='App'>
       <BrowserRouter>
         <UserStorage>
           <Header />
-          <Routes>
 
-            <Route path="/" element={ <Home /> } />
-            <Route path="login/*" element={ <Login /> } />
-            <Route
-              path="conta/*"
-              element={
-                <ProtectedRouter>
-                  <User />
-                </ProtectedRouter>
-              }
-            />
-            <Route path="foto/:id" element={ <Photo /> } />
-            <Route path="perfil/:user" element={ <UserProfile /> } />
-            <Route path="*" element={ <NotFound /> } />
-
-          </Routes>
+          <main className='AppBody'>
+            <Routes>
+              <Route path="/" element={ <Home /> } />
+              <Route path="login/*" element={ <Login /> } />
+              <Route
+                path="conta/*"
+                element={
+                  <ProtectedRouter>
+                    <User />
+                  </ProtectedRouter>
+                }
+              />
+              <Route path="foto/:id" element={ <Photo /> } />
+              <Route path="perfil/:user" element={ <UserProfile /> } />
+              <Route path="*" element={ <NotFound /> } />
+            </Routes>
+          </main>
 
           <Footer />
         </UserStorage>
       </BrowserRouter>
 
-    </>
+    </div>
   )
 }
 
--- dogs/src/components/Footer/Footer.jsx
+++ dogs/src/components/Footer/Footer.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import styles from './Footer.module.css'
+import Dogs from '../../assets/dogs-footer.svg?react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className={styles.footer}>
+      <Dogs />
+      <p>Dogs. Alguns direitos reservados</p>
+    </footer>
   )
 }
 
